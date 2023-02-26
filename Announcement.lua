824443e9ed8e108f35b2ab88c1bd46a814e5aa4b
JamesKnight0001 <80499782+JamesKnight0001@users.noreply.github.com>
2023-02-26 00:03:08 -0800

    Create Announcement.lua

---
+++ Announcement.lua
@@ -0,0 +1,3 @@
+CoreGui:SetCore("SendNotification", {Title = "Announcement",Text = "I created a test anime-website, please check it out if u have time.", Duration = math.huge})
+CoreGui:SetCore("SendNotification", {Title = "Announcement",Text = "Automaticly copied.", Duration = math.huge})
+setclipboard("http://jamesdexanime.epizy.com/home")
