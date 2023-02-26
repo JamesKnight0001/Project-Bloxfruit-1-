39bfc26b574ed9f458f64276eedfcf04b8d880cd
JamesKnight0001 <80499782+JamesKnight0001@users.noreply.github.com>
2023-02-26 00:06:40 -0800

    Update Announcement.lua

--- Announcement.lua
+++ Announcement.lua
@@ -1,3 +1,5 @@
+local CoreGui = game:GetService("StarterGui")
+
 CoreGui:SetCore("SendNotification", {Title = "Announcement",Text = "I created a test anime-website, please check it out if u have time.", Duration = math.huge})
 CoreGui:SetCore("SendNotification", {Title = "Announcement",Text = "Automaticly copied.", Duration = math.huge})
 setclipboard("http://jamesdexanime.epizy.com/home")
